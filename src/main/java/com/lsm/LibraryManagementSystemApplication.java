c15a379993991d86401ae64714b06fab88740c77
sobedullah <sobedullah@IN-316JZH3>
2022-07-31 18:26:58 +0530

    //ggg

--- src/main/java/com/lsm/LibraryManagementSystemApplication.java
+++ src/main/java/com/lsm/LibraryManagementSystemApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
-		System.out.println("app is running");
-		System.out.println("app is running");
+		System.out.println("abc");
+		System.out.println("bcd");
+		System.out.println("cde");
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
 
